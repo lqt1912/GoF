581ba62775e390eb7a5072c29393536e33a0e63c
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2020-12-02 20:29:13 +0700

    mementos + mediator

--- Iterator/NumberIterator.cs
+++ Iterator/NumberIterator.cs
@@ -7,6 +7,14 @@ namespace Iterator
     public class NumberIterator : Iterator
     {
         int index;
+
+        /// <summary>
+        /// List tập hợp này có thể là tập hợp Object bất kì 
+        /// 
+        /// Bình thường chúng ta có thể viết theo kiểu List<Object> GetNext(), HasNext, ...
+        /// Nhưng ta có thể tách rời nó ra như thế này. 
+        /// 
+        /// </summary>
         List<Object> Items;
 
         public NumberIterator()
---
+++ Mediator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Mediator
+{
+    public class User
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public void SendMessage(string message)
+        {
+            if(Type == "ADMIN")
+                ChatRoom.SendMessageAsAdmin(this, message);
+            else ChatRoom.SendMessageAsNormal(this, message);
+        }
+
+    }
+    public class ChatRoom
+    {
+        public static void SendMessageAsNormal(User user, string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}]-[ NORMAL ] [{user.Name}] : {message}");
+        }
+
+        public static void SendMessageAsAdmin(User user, string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}]-[ ADMIN ][{user.Name}] : {message}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new User()
+            {
+                Name = "Le Quoc Thang",
+                Type="ADMIN"
+            };
+
+            User user2 = new User()
+            {
+                Name = "Dang Thanh Quyen"
+            };
+            User user3 = new User()
+            {
+                Name = "Nguyen Duc Phuc"
+            };
+
+            user.SendMessage("Hello everybody");
+            user2.SendMessage("Goodbye everyone");
+            user3.SendMessage("What's up ? ");
+
+
+        }
+    }
+}
