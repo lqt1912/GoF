f45f378ad581957a49c0814c93075e8637c1ba47
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2020-12-04 09:10:54 +0700

    change sth

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -2,26 +2,35 @@
 
 namespace Mediator
 {
-    public class User
+    public abstract class IUser 
     {
         public string Type { get; set; }
         public string Name { get; set; }
-        public void SendMessage(string message)
+        public abstract void SendMessage(string message);
+    }
+    public class User:IUser
+    {
+        public override void SendMessage(string message)
         {
-            if(Type == "ADMIN")
-                ChatRoom.SendMessageAsAdmin(this, message);
-            else ChatRoom.SendMessageAsNormal(this, message);
+            ChatRoom.SendMessageAsNormal(this, message);
         }
+    }
 
+    public class Admin : IUser
+    {
+        public override void SendMessage(string message)
+        {
+            ChatRoom.SendMessageAsAdmin(this, message);
+        }
     }
     public class ChatRoom
     {
-        public static void SendMessageAsNormal(User user, string message)
+        public static void SendMessageAsNormal(IUser user, string message)
         {
             Console.WriteLine($"[{DateTime.Now}]-[ NORMAL ] [{user.Name}] : {message}");
         }
 
-        public static void SendMessageAsAdmin(User user, string message)
+        public static void SendMessageAsAdmin(IUser user, string message)
         {
             Console.WriteLine($"[{DateTime.Now}]-[ ADMIN ][{user.Name}] : {message}");
         }
@@ -31,17 +40,17 @@ namespace Mediator
         static void Main(string[] args)
         {
 
-            User user = new User()
+            IUser user = new Admin()
             {
                 Name = "Le Quoc Thang",
                 Type="ADMIN"
             };
 
-            User user2 = new User()
+            IUser user2 = new User()
             {
                 Name = "Dang Thanh Quyen"
             };
-            User user3 = new User()
+            IUser user3 = new User()
             {
                 Name = "Nguyen Duc Phuc"
             };
